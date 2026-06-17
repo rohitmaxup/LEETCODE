043fad8259a7890108920ce4c05bf8e8a8467465
Rohit Zunzunwala <zunzunwalarohit@gmail.com>
2026-06-17 12:25:47 +0530

    Time: 1 ms (86.32%) | Memory: 19.2 MB (98.43%) - LeetSync

---
+++ 1469-minimum-number-of-steps-to-make-two-strings-anagram/minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minSteps(string s, string t) {
+        int n = s.length();
+        int mp[26]={0};
+
+        for(int i = 0 ; i<n ; i++)
+        {
+            mp[s[i]-'a']++;
+            mp[t[i]-'a']--;
+        }
+
+        int result =0;
+
+        for(int i = 0 ; i<26 ; i++)
+        {
+            if(mp[i]>0)
+            {
+                result +=mp[i];
+            }
+        }
+        return result;
+    }
+};
