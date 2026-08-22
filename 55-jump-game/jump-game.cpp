913bd7b450bf4dec1678e4554ff3a5fd5ba7251f
Rohit Zunzunwala <zunzunwalarohit@gmail.com>
2026-08-23 00:36:00 +0530

    Time: 0 ms (100.00%) | Memory: 52.3 MB (83.32%) - LeetSync

---
+++ 55-jump-game/jump-game.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int maxIdx = 0;
+        for(int i = 0 ; i < nums.size(); i++)
+        {
+        if(i>maxIdx) return false;
+        maxIdx = max(maxIdx, i + nums[i]);
+        }
+
+        return true;
+  }
+};
